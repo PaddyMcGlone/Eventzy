ef2df11074e6c099699d9147e5e19f833fc64068
Paddy McGlone <paddymcglone@gmail.com>
2019-10-14 18:46:30 +0100

    Adding the initial commit.

---
+++ Eventzy/Models/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventzy.Models
+{
+    public class ApplicationContext: DbContext
+    {
+        #region Constructor
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+        #endregion
+
+        #region Properties
+
+        public DbSet<Event> Events { get; set; }
+
+        #endregion
+    }
+}
---
+++ Eventzy/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Eventzy.Models
+{
+    public class Event
+    {
+        #region
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }        
+
+        #endregion        
+    }
+}
